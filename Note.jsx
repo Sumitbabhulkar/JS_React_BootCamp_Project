b3965b43e73b32d854e80c01d05e82dbcbf1a215
Sumitbabhulkar <sumitbabhulkar29@gmail.com>
2021-06-05 16:09:51 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>JavaScript and React.js</h1>
+            <p>
+            React is a JavaScript library for building user interfaces.
+
+            React is used to build single page applications.
+
+            React allows us to create reusable UI components.
+
+
+            </p>
+        </div>
+    )
+}
+
+export default Note;
